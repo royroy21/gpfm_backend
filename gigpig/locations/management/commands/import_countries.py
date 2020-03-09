fcf67a54fe50b7afda40bb845558ac9d054a3715
Roy Hanley <royhanley8@gmail.com>
2020-03-09 01:50:51 +0000

    Import countries now uses csv module

--- gigpig/locations/management/commands/import_countries.py
+++ gigpig/locations/management/commands/import_countries.py
@@ -1,5 +1,5 @@
+import csv
 from django.core.management.base import BaseCommand
-
 from gigpig.locations import models
 
 
@@ -10,19 +10,14 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open(self.COUNTRIES_FILE) as f:
             countries = [
-                models.Country(**self.get_country_name_and_code(country_data))
-                for country_data
-                in f.readlines()
+                models.Country(**{
+                    "name": name,
+                    "code": code,
+                })
+                for name, code in csv.reader(f)
             ]
             self.stdout.write(self.style.SUCCESS(
                 f"creating {len(countries)} new countries"
             ))
             models.Country.objects.bulk_create(countries)
             self.stdout.write(self.style.SUCCESS("finished"))
-
-    def get_country_name_and_code(self, country_data):
-        name, code = country_data.split(",")
-        return {
-            "name": name,
-            "code": code.rstrip("\n"),
-        }
